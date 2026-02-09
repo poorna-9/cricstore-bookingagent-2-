1c213688e1d5e68ee16b288da1a79324ab587ebe
poorna-9 <166108155+poorna-9@users.noreply.github.com>
2026-02-09 19:07:03 +0530

    Update settings.py

--- cricketstore/settings.py
+++ cricketstore/settings.py
@@ -127,6 +127,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
