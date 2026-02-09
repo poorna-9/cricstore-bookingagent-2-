918cc7146d72ef497de732873fb2e2725dca73ae
poorna-9 <166108155+poorna-9@users.noreply.github.com>
2026-02-09 19:38:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
+#!/bin/sh
+echo "Applying migrations..."
 python manage.py migrate
+
+echo "Collecting static files..."
 python manage.py collectstatic --noinput
-gunicorn cricketstore.wsgi:application --bind 0.0.0.0:$PORT
+
+echo "Starting Gunicorn..."
+gunicorn cricketstore.wsgi:application --bind 0.0.0.0:$PORT --workers 3 --log-level info
