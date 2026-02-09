65c8fe2a727c1839c00c89cf0fbf24a4528ce840
poorna-9 <166108155+poorna-9@users.noreply.github.com>
2026-02-09 16:38:50 +0530

    Update settings.py

--- cricketstore/settings.py
+++ cricketstore/settings.py
@@ -75,14 +75,13 @@ WSGI_APPLICATION = 'cricketstore.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', 'cricketstore'),
-        'USER': os.environ.get('DB_USER', 'postgres'),
-        'PASSWORD': os.environ.get('DB_PASS', 'postgres'),
-        'HOST': os.environ.get('DB_HOST', 'db'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
+        'HOST': os.environ.get('DB_HOST'),
         'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
-
 
 
 # Password validation
